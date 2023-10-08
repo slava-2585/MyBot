4e1f8025ae379adf6aaf1966d842fc282e204e15
Nikiforov.Vyacheslav <slava-2585@yandex.ru>
2023-10-08 19:51:44 +0300

    Вывод анкет

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ vk_session = vk_api.VkApi(token=token)
 longpoll: VkLongPoll = VkLongPoll(vk_session)
 
 
-def send_mes(user_id, text, keyboard=None):
+def send_mes(user_id, text, keyboard=None, attachment=None):
     param = {
         'user_id': user_id,
         'message': text,
@@ -16,6 +16,8 @@ def send_mes(user_id, text, keyboard=None):
     }
     if keyboard != None:
         param['keyboard'] = keyboard.get_keyboard()
+    if attachment != None:
+        param['attachment'] = attachment
     vk_session.method('messages.send', param)
 
 
@@ -23,57 +25,75 @@ def main():
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             mes = event.text.lower()
-            id = event.user_id
-            print(mes)
             if mes in ['начать', 'start']:
                 keyboard = VkKeyboard(one_time=True)
                 keyboard.add_button('Да')
                 keyboard.add_button('Нет')
                 keyboard.add_button('Помощь')
-                send_mes(id, 'Привет! Это бот Love Bot. Запустить поиск пары?', keyboard)
-                second_main(id)
+                send_mes(event.user_id, 'Привет! Это бот Love Bot. Запустить поиск пары?', keyboard)
+                second_main()
             else:
-                send_mes(id, 'Как дела? Хотите просто поболтать?')
+                send_mes(event.user_id, 'Как дела? Хотите просто поболтать?')
 
 
-def second_main(id):
+def second_main():
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             mes = event.text.lower()
-            #id = event.user_id
             if mes == 'да':
-                search_love(id)
+                send_mes(event.user_id, 'Введите Имя')
+                search_love()
             if mes == 'нет':
-                send_mes(id, 'Очень жаль. Досвидания.')
+                send_mes(event.user_id, 'Очень жаль. Досвидания.')
                 main()
             if mes == 'Помощь':
-                send_mes(id, 'Пока в разработке')
+                send_mes(event.user_id, 'Пока в разработке')
             else:
                 main()
 
 
-def search_love(id):
+def search_love():
     name: str = None
     city: str = None
-    age: int = None
+    age: str = None
     sex: int = None
+    age_from: int
+    age_to: int
 
-    send_mes(id, 'Введите Имя')
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            id = event.user_id
             mes = event.text.lower()
             if name == None:
                 name = mes
-                send_mes(id, 'Введите Город')
+                send_mes(event.user_id, 'Введите Город')
                 continue
             if name != None and city == None:
                 city = mes
-                send_mes(id, 'Введите Возраст')
+                send_mes(event.user_id, 'Введите Возраст')
                 continue
             if name != None and city != None and age == None:
-                age = int(mes) # Сделать обработчик
-                send_mes(id, 'Введите Пол (Муж/Жен)')
-                continue
+                age = mes  # Сделать обработчик
+                try:
+                    age_from, age_to = age.split("-")
+                except ValueError:
+                    send_mes(event.user_id, 'Неправильно введен возраст, введите заново)')
+                    age = None
+                    continue
+                try:
+                    age_from = int(age_from)
+                except ValueError:
+                    send_mes(event.user_id, 'Неправильно введен возраст, введите заново)')
+                    age = None
+                    continue
+                try:
+                    age_to = int(age_to)
+                except ValueError:
+                    send_mes(event.user_id, 'Неправильно введен возраст, введите заново)')
+                    age = None
+                    continue
+
+                send_mes(event.user_id, 'Введите Пол (Муж/Жен)')
             if name != None and city != None and age != None and sex == None:
                 if mes == 'муж':
                     sex = 2
@@ -81,23 +101,44 @@ def search_love(id):
                     sex = 1
                 else:
                     sex = 0
-                if name != None and city != None and age != None and sex != None:
-                    # item = iter(search_user(name, age, sex, city))
-                    # user = next(item)
-                    # send_mes(id, f'Первая пара {user["first_name"]}')
-                    view_user(id, name, age, sex, city)
+
+            if name != None and city != None and age != None and sex != None:
+                candidate = iter(search_user(name, age_from, age_to, sex, city))
+                view_user(candidate)
 
 
-
-def view_user(id, name, age, sex, city):
-    #print(name, age, sex, city)
-    item = iter(search_user(name, age, sex, city))
-    user = next(item)
-    # print(user)
-    send_mes(id, f'Первая пара {user["first_name"]}, {user["last_name"]}')
-    # for event in longpoll.listen():
-    #     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-
+def view_user(candidate):
+    keyboard1 = VkKeyboard()
+    keyboard1.add_button('Лайк')
+    keyboard1.add_button('Дизлайк')
+    keyboard1.add_line()
+    keyboard1.add_button('Дальше')
+    keyboard1.add_button('Остановить')
+    for event in longpoll.listen():
+        attachment: list = []
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            try:
+                item = next(candidate)
+            except StopIteration:
+                send_mes(event.user_id, 'Ничего не найдено или список закончился. Приходите снова.')
+                main()
+            mes = event.text.lower()
+            if mes == 'лайк':
+                pass
+            elif mes == 'дизлайк':
+                pass
+            elif mes == 'дальше':
+                try:
+                    item = next(candidate)
+                except StopIteration:
+                    send_mes(event.user_id, 'Пока все. Будем рады Вас видеть снова.')
+            elif mes == 'остановить':
+                send_mes(event.user_id, 'Пока. Будем рады видеть Вас снова.')
+                main()
+            print(attachment)
+            send_mes(event.user_id, f'{item['name']} День рождения:{item['date_birth']} Город: {item['city']})', keyboard1)
+            for i in range(len(item['photo'])):
+                send_mes(event.user_id, f'Фото {str(i + 1)}', keyboard1, f'photo{item['id']}_{item['photo'][i]}')
 
 
 if __name__ == '__main__':
--- vk.py
+++ vk.py
@@ -4,7 +4,7 @@ import vk_api
 from key import main_token
 
 
-def search_user(name: str, age: int, sex: int, city: str):
+def search_user(name: str, age_from: int, age_to: int, sex: int, city: str):
     # with open('token.txt', 'r') as file_object:
     #     token = file_object.readline().strip()
 
@@ -16,21 +16,22 @@ def search_user(name: str, age: int, sex: int, city: str):
         'is_closed': False,
         'fields': 'city, bdate',
         'q': name,
-        'age_from': age,
-        'age_to': age,
+        'age_from': age_from,
+        'age_to': age_to,
         'sex': sex,
         'has_photo': 1,
         'online': 1,
+        'hometown': city,
         'sort': 0
             }
     session = vk_api.VkApi(token=main_token)
     result = session.method('users.search', params)['items']
     for item in result:
         dic_user = {}
-        if item['is_closed'] or 'city' not in item:
+        if item['is_closed']:
             continue
-        if item['city']['title'].lower() != city.lower():
-            continue
+        # if item['city']['title'].lower() != city.lower():
+        #     continue
         params_photo = {
             'owner_id': item['id'],
             'album_id': 'profile',
@@ -40,8 +41,8 @@ def search_user(name: str, age: int, sex: int, city: str):
         photo_user = session.method('photos.get', params_photo)['items']
         sort_photo = sorted(photo_user, key=lambda x: x['likes']['count'], reverse=True)
         dic_user['id'] = item['id']
-        dic_user['first_name'] = item['first_name']
-        dic_user['last_name'] = item['last_name']
+        dic_user['name'] = f'{item['first_name']} {item['last_name']}'
+        #dic_user['last_name'] = item['last_name']
         dic_user['city'] = city
         if item['bdate']:
             dic_user['date_birth'] = item['bdate']
